29152451d1a55c9939bfbfb280504601c5e666b7
Cristian Azano <cmichel529@gmail.com>
2025-02-24 17:15:40 -0600

    Landing y formulario

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,27 @@
 <?php
 
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::get('/home', function () {
+    return view('home');
+});
+
+Route::get('/contact', function () {
+    return view('contact');
+});
+                                        // Request  //nombre de la variable (libre)
+Route::post('/crear-contacto', function (Request $request) {
+    dd($request->all(), $request->mail);
+
+    // Validar formulario
+
+    //Guardar a DB
+
+    // Redirigir
+
 });
